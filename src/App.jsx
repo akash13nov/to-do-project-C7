b483d46493150efd7d8856bdb1016c0a45d46e7e
akash13nov <akash13nov@gmail.com>
2025-12-28 23:26:47 +0530

    Class-7

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Form from "./components/Form";
+import Card from "./components/Card";
+
+const App = () => {
+  return (
+    <div>
+      <Form />
+      <Card />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Form = () => {
+  return <div className="text-white bg-amber-900 flex">this is form</div>;
+};
+
+export default Form;
